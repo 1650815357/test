e707086126c0345616ad4e8783b65e4ff01d6874
liuwu <1650815357@qq.com>
2019-12-16 11:37:56 +0800

    sad

---
+++ testSpringMvc/src/main/java/com/project/entity/User.java
@@ -0,0 +1,59 @@
+package com.project.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="sys_user")
+public class User {
+	@Id
+	@GeneratedValue()
+	@Column(name="id")
+	private int id;
+	
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="phone")
+	private String phone;
+	
+	@Column(name="password")
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
